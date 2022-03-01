13e732c49f26ac77a538703f3622f40237d9ee0d
Arslan240 <arslan.jamil1322@gmail.com>
2022-03-01 12:09:25 +0500

    Started and completed Layout and simple styling

---
+++ FAQ Section/script.js
@@ -0,0 +1,14 @@
+/*  ================
+      ICONS for later injecting them into html
+    ================ */
+
+    const chevron_up = `<i class="fa-solid fa-chevron-up"></i>`;
+    const chevron_down = `<i class="fa-solid fa-chevron-down"></i>`;
+
+    const question_div = `
+    <div class="question-div">
+    <span class="question">${question}</span>
+    <i class="fa-solid fa-chevron-up"></i>
+    <p class="answer">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus quibusdam voluptas maiores reiciendis culpa dolore rem consectetur non commodi ullam, officia mollitia tempora adipisci</p>
+    </div>
+    `;
